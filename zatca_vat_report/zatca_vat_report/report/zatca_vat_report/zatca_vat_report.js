5d6782fffb0887ae5bbf3e006818d55b5ba7cb8e
Ramees-k <ramees@enfono.com>
2026-02-25 13:44:53 +0300

    Added company filter

--- zatca_vat_report/zatca_vat_report/report/zatca_vat_report/zatca_vat_report.js
+++ zatca_vat_report/zatca_vat_report/report/zatca_vat_report/zatca_vat_report.js
@@ -4,18 +4,26 @@
 frappe.query_reports["ZATCA VAT Report"] = {
 	"filters": [
 		{
+			"fieldname": "company",
+			"label": "Company",
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd": 0,
+			"description": "Leave blank to show all companies combined"
+		},
+		{
 			"fieldname": "from_date",
 			"label": "From Date",
 			"fieldtype": "Date",
 			"width": 100,
-			"reqiured": 1
+			"reqd": 1
 		},
 		{
 			"fieldname": "to_date",
 			"label": "To Date",
 			"fieldtype": "Date",
 			"width": 100,
-			"reqiured": 1
+			"reqd": 1
 		},
 	]
 };
